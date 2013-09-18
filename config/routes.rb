eeee4b76c8a935225313fba437fca66a7acd1a88
Ole Johan Væringstad <olejohan@varingstad.com>
2013-09-18 21:02:25 +0200

    Modify Signup/New Farmer form

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Wolfbane::Application.routes.draw do
 
   resources :farmers
 
+  match '/signup', to: 'farmers#new', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
