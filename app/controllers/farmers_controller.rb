a7b8e7ced5427a8c077a839a6c8ec71751af21d2
Ole Johan Væringstad <olejohan@varingstad.com>
2013-09-22 04:55:43 +0200

    Change signup message

--- app/controllers/farmers_controller.rb
+++ app/controllers/farmers_controller.rb
@@ -28,7 +28,7 @@ class FarmersController < ApplicationController
 
     respond_to do |format|
       if @farmer.save
-        format.html { redirect_to @farmer, notice: 'Farmer was successfully created.' }
+        format.html { redirect_to @farmer, notice: 'Welcome to Wolfbane. May your sheep live long and prosper.' }
         format.json { render action: 'show', status: :created, location: @farmer }
       else
         format.html { render action: 'new' }
