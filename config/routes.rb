0f8fc8e41ce50632337df7b5725bc8b60901f610
Ole Johan Væringstad <olejohan@varingstad.com>
2013-09-22 05:12:28 +0200

    Set Login page as Main page

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Wolfbane::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
