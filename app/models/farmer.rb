7bb86d84b6c87943c4eee3955d8d555f12a2f5f4
Ole Johan Væringstad <olejohan@varingstad.com>
2013-09-22 04:46:27 +0200

    Add validation of Backup as existing user

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,6 +1,9 @@
 class Farmer < ActiveRecord::Base
   has_many :sheep
-  before_save { self.email = email.downcase }
+  before_save {
+    self.email = email.downcase
+    self.backup = backup.downcase
+  }
   validates :name, presence: true, length: { maximum: 50 }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   PHONE_REGEX = /\A\+47[0-9]{8}\z/
@@ -11,4 +14,11 @@ class Farmer < ActiveRecord::Base
                     format: { with: PHONE_REGEX }
   has_secure_password
   validates :password, length: { minimum: 8 }
+  validate :valid_backup
+
+  def valid_backup
+    if not (self.backup.nil? || self.backup.blank? || Farmer.find_by_email(backup))
+      errors.add(:backup, 'no such email registrated')
+    end
+  end
 end
