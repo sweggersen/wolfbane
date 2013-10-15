71348787bb7ff9f9c261ed7bff3d28063d0b6ab7
Ole Johan Væringstad <olejohan@varingstad.com>
2013-10-15 19:16:57 +0200

    Set signup as root page

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Wolfbane::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sessions#new'
+  root 'farmers#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
