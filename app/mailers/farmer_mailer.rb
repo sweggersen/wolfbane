2d859fa61d59a2b3a7e765af4429c9a28c682e05
Ole Johan Væringstad <olejohan@varingstad.com>
2013-10-17 11:13:01 +0200

    Add simple alert mails

--- app/mailers/farmer_mailer.rb
+++ app/mailers/farmer_mailer.rb
@@ -1,8 +1,9 @@
 class FarmerMailer < ActionMailer::Base
   default from: "gruppe20@wolfbane.com"
 
-  def alert_email(farmer)
+  def alert_email(farmer, sheep)
     @farmer = farmer
+    @sheep = sheep
     @url = 'http://wolfbane.heroku.com'
     mail(to: @farmer.email, subject: 'Your sheep is under attack!')
   end
