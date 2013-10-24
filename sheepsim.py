9d7cb7b12e102fc497ad6d70b79ef6f87ea4e6f0
DeimosA <krvolden@gmail.com>
2013-10-24 15:02:24 +0200

    Commented code

--- sheepsim.py
+++ sheepsim.py
@@ -3,62 +3,81 @@ from time import sleep
 from random import random
 
 
-global sleepTime
 global positions
-global startPos
-global maxLat
-global minLat
-global minLong
-global maxLong
-global maxMove
+positions = {} #dictionary for previous position. ID as key, tuple of lat, long as value
+
+#define global constants
+global SLEEP_TIME
+#boundary box
+global MAX_LAT
+MAX_LAT = 63.726403
+global MIN_LAT
+MIN_LAT = 63.660369
+global MIN_LONG
+MIN_LONG = 11.443253
+global MAX_LONG
+MAX_LONG = 11.536636
+global MAX_MOVE #max lat or long variance per position update (0.01 = ~500m)
+MAX_MOVE = 0.01
+global START_POS #center of area where sheep will be released
+START_POS = (MAX_LAT - MAX_MOVE, MIN_LONG + MAX_MOVE)
 
 
-positions = {}
-maxLat = 63.726403
-minLat = 63.660369
-minLong = 11.443253
-maxLong = 11.536636
-maxMove = 0.01 #~500 meter
-startPos = (maxLat - maxMove, minLong + maxMove)
 
-
+#calculate new position with random variance within boundary box
 def newPosition(pos, minimum, maximum):
     newPos = 0
-    while newPos < minimum or newPos > maximum:
-        newPos = pos + (maxMove - random() * maxMove * 2)
+    while newPos < minimum or newPos > maximum: #redo if new position is outside boundary box
+        newPos = pos + (MAX_MOVE - random() * MAX_MOVE * 2)
     return newPos
 
 
+#do actual transmission of form data
+def transmit(ID, lat, lon):
+    pass
+
+
+#do per sheep
 def sendPosition(sheepID):
-    currentLat, currentLong = positions.get(sheepID, startPos)
+    #get previous position or START_POS as default
+    currentLat, currentLong = positions.get(sheepID, START_POS)
     
-    newLat = newPosition(currentLat, minLat, maxLat)
-    newLong = newPosition(currentLong, minLong, maxLong)
+    #calculate new positions
+    newLat = newPosition(currentLat, MIN_LAT, MAX_LAT)
+    newLong = newPosition(currentLong, MIN_LONG, MAX_LONG)
     
+    #store new position for next iteration
     positions[sheepID] = (newLat, newLong)
+    #transmit(sheepID, newLat, newLong)
     
     print 'new position for sheep nr ' + str(sheepID) + ' is ' + "%.6f" % newLat + ', ' + "%.6f" % newLong
-    #sleep(sleepTime)
+    #sleep(SLEEP_TIME)
     sleep(0.5)
 
 
+#main program loop
 def main(ranges):
     for item in ranges:
+        #check for single sheep ID or range of ID's
         if len(item) == 1:
             sendPosition(item[0])
         else:
             n = item[1] - item[0] +1
+            #iterate over ID range
             for i in range(n):
                 sendPosition(item[0]+i)
 
 
+#initial setup
 def init():
     print "\nWolfBane sheepsim 2014\n"
 
-    ranges = []
-    n = 0
+    ranges = [] #array of sheep ranges
+    n = 0 #total number of sheep
+    #read from input file (provided to stdin). Example command: python sheepsim.py < ranges.txt
     for line in stdin:
         temp = line.strip().split('-')
+        #check for single sheep ID or range of ID's
         if len(temp) == 1:
             temp[0] = int(temp[0])
             n += 1
@@ -69,15 +88,17 @@ def init():
 
         ranges.append(temp)
 
-    global sleepTime
-    sleepTime = 86400.0 / (n*3)
+    #calculate time between updates based on total number of sheep
+    global SLEEP_TIME
+    SLEEP_TIME = 86400.0 / (n*3)
 
     print 'There are ' + str(n) + ' sheep to update'
-    print 'Will sleep for ' + str(sleepTime) + 's between updates'
+    print 'Will sleep for ' + str(SLEEP_TIME) + 's between updates'
     for item in ranges:
         print item
     print
 
+    #loop program for as long as needed. Use ctrl-c to abort
     while True:
         main(ranges)
 
