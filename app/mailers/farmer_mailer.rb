3c182705d5c09f08c8f49ef24f87d15aaa10ed7c
Ole Johan Væringstad <olejohan@varingstad.com>
2013-11-14 15:57:54 +0100

    Update mailer message to include position on map

--- app/mailers/farmer_mailer.rb
+++ app/mailers/farmer_mailer.rb
@@ -4,7 +4,7 @@ class FarmerMailer < ActionMailer::Base
   def alert_email(farmer, sheep)
     @farmer = farmer
     @sheep = sheep
-    @url = 'http://wolfbane.heroku.com'
+    @url = "http://#{(Rails.env.development? ? 'localhost:3000' : 'wolfbane.heroku.com')}"
     mail(to: @farmer.email, subject: 'Your sheep is under attack!')
   end
 end
