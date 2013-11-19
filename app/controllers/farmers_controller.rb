005c3c65954e3f513ae6b7ebcd27c8168584fcba
Ole Johan Væringstad <olejohan@varingstad.com>
2013-11-19 11:31:51 +0100

    Change farmer ordering to asc

--- app/controllers/farmers_controller.rb
+++ app/controllers/farmers_controller.rb
@@ -5,7 +5,7 @@ class FarmersController < ApplicationController
 
   # Listing all farmers, paginated with 30 listings per page
   def index
-    @farmers = Farmer.order('email DESC').paginate(page: params[:page], per_page: 10)
+    @farmers = Farmer.order('email ASC').paginate(page: params[:page], per_page: 10)
   end
 
   def show
